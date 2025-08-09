4831bca197ec56e8dd0463574da88b3e64f59a27
JuanCNav <juancnavob@gmail.com>
2025-08-09 12:54:53 -0400

    Day 2 exeercises

---
+++ day2age.py
@@ -0,0 +1,28 @@
+
+print(3 != 3)
+print((8 == 8) or (5 > 10))
+
+
+x=10
+y=3
+print(x%y)
+
+age=int (input("Hey friend how old are you?"))
+
+if age<13:
+    print("Access Denied")
+elif age<18:
+    print("Access granted to teen zone")
+elif age>=18:
+    print("Access granted to adult zone")
+
+
+
+temp= int (input("hey what is the temperature right now?"))
+
+
+
+if temp > 20:
+    print("Its getting pretty hot")
+elif temp<= 20:
+    print("Its getting chilly")
