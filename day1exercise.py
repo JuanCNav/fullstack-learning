e93d58ffab90156fbb498aed3c814b08b6cdddd1
JuanCNav <juancnavob@gmail.com>
2025-08-08 23:00:19 -0400

    day1exercise.py

---
+++ day1exercise.py
@@ -0,0 +1,29 @@
+
+
+name=input("What is your name?")
+food=input("What is your favorite food?")
+
+print("Hello,",(name),"I like",(food),"too!")
+
+
+
+print("Hey lets do some math!")
+
+x=int (input("Give me one number"))
+y=int (input("Give me another number"))
+r="is equal to"
+print("Ok following I will attach sum, rest, multiplication and division results of those numbers")
+print("Sum of",(x),"plus",(y),(r),(x+y))
+print("Rest of", (x),"minus",(y),(r),(x-y))
+print("Multiplication of",(x),"times",(y),(r),(x*y))
+print("Division of",(x),"by",(y),(r),(x/y))
+
+print("Hope this results help!")
+
+
+birth_year=int(input("When were you born?"))
+current_year=2025
+print("That means you are currently",(current_year-birth_year),"years old.")
+
+
+
