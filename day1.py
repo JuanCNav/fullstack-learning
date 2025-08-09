b245a3b6da989d1836731b60c9636edb3681c5c6
JuanCNav <juancnavob@gmail.com>
2025-08-08 22:43:28 -0400

    Day 1 - Python basics practice

---
+++ day1.py
@@ -0,0 +1,28 @@
+x=10
+y=3
+print(x+y)
+print(x/y)
+print(x%y)
+print(x**y)
+
+
+
+
+name= "juan"
+age=23
+height_inches=75
+height_cm= height_inches*2.54
+is_student= True
+
+print(name,age,height_cm)
+print("What is his occupation? is he a student?")
+print(is_student)
+print(type(name))
+
+
+Name2=input("What is your name?")
+
+Age2=int(input("How old are you?"))
+
+print("Hello,",Name2)
+print("My age is",Age2)
